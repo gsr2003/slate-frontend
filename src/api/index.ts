bece78944d177b714c8f6c0cddab63202fcedf0c
gsr2003 <iamgsr1206@gmail.com>
2026-08-10 22:31:39 +0530

    Update API URL for production

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://slate-backend-0aky.onrender.com/api",
 });
 
 // Request ke saath token automatically bhejne ke liye
